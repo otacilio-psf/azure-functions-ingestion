4b2947fb35a5fc7674a450b80d9b6d7ac178ac45
Otacilio Filho <otaciliopedro@gmail.com>
2022-09-10 22:27:01 +0100

    Change terraform to be more automated

---
+++ IaC/22_key_vault_secrets.tf
@@ -0,0 +1,49 @@
+resource "azurerm_key_vault_secret" "datalake_access_sp_app_id" {
+  name         = "sp-datalake-app-id"
+  value        = azuread_application.dl_contributor.application_id
+  key_vault_id = azurerm_key_vault.akv.id
+
+  depends_on = [
+    azurerm_key_vault_access_policy.deployer
+  ]
+}
+
+resource "azurerm_key_vault_secret" "datalake_access_sp_obj_id" {
+  name         = "sp-datalake-obj-id"
+  value        = azuread_service_principal.dl_contributor.object_id
+  key_vault_id = azurerm_key_vault.akv.id
+
+  depends_on = [
+    azurerm_key_vault_access_policy.deployer
+  ]
+}
+
+resource "azurerm_key_vault_secret" "datalake_access_sp_secret" {
+  name         = "sp-datalake-secret"
+  value        = azuread_application_password.dl_contributor.value
+  key_vault_id = azurerm_key_vault.akv.id
+
+  depends_on = [
+    azurerm_key_vault_access_policy.deployer
+  ]
+}
+
+resource "azurerm_key_vault_secret" "datalake_access_sp_tenant" {
+  name         = "sp-datalake-tenant"
+  value        = data.azurerm_client_config.current.tenant_id
+  key_vault_id = azurerm_key_vault.akv.id
+
+  depends_on = [
+    azurerm_key_vault_access_policy.deployer
+  ]
+}
+
+resource "azurerm_key_vault_secret" "datalake_path" {
+  name         = "datalake-path"
+  value        = "abfss://${azurerm_storage_data_lake_gen2_filesystem.datalake.name}@${azurerm_storage_account.stga_datalake.name}.dfs.core.windows.net"
+  key_vault_id = azurerm_key_vault.akv.id
+
+  depends_on = [
+    azurerm_key_vault_access_policy.deployer
+  ]
+}
--- IaC/99_output.tf
+++
@@ -1,4 +0,0 @@
-output "rg_name" {
-  value       = azurerm_resource_group.project.name
-  description = "RG name"
-}
